33cf6c00f761ff85c8dc040ce3b52f08bb264030
Prameesh <prameepramee0@gmail.com>
2023-01-04 22:33:12 +0530

    Database connection added

--- datebase/databaseConnection.go
+++ datebase/databaseConnection.go
@@ -1,1 +1,26 @@
 package datebase
+
+import (
+	_ "github.com/jackc/pgx/v5"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+	"os"
+)
+
+var (
+	host     = os.Getenv("HOST")
+	port     = os.Getenv("ADDR")
+	user     = os.Getenv("USER")
+	password = os.Getenv("PASSWORD")
+	dbName   = os.Getenv("DBNAME")
+)
+
+func DBConnection() *gorm.DB {
+	db, err := gorm.Open("pgx", "host=%s port=%s dbname=%s user=%s password=%s", host, port, user, password, dbName)
+	if err != nil {
+		log.Fatalf("Unable to connect Database %v \n", err)
+	}
+	defer db.Close()
+	return db
+}
